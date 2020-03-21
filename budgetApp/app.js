2d6d5e62f35de26589f6d79ab27e6cb6294ad869
GO Lee <52603436+goleedev@users.noreply.github.com>
2020-03-21 23:27:57 +0900

    Add reading data

--- budgetApp/app.js
+++ budgetApp/app.js
@@ -3,25 +3,38 @@ var budgetController = (function() {})();
 
 // UI CONTROLLER
 var UIController = (function() {
+  var DOMstrings = {
+    inputType: ".add__type",
+    inputDescription: ".add__description",
+    inputValue: ".add__value",
+    inputBtn: ".add__btn"
+  };
+
   return {
     getinput: function() {
       return {
-        type: document.querySelector(".add__type").value,
-        description: document.querySelector(".add__description").value,
-        value: document.querySelector(".add__value").value
+        type: document.querySelector(DOMstrings.inputType).value,
+        description: document.querySelector(DOMstrings.inputDescription).value,
+        value: document.querySelector(DOMstrings.inputValue).value
       };
+    },
+
+    getDOMstrings: function() {
+      return DOMstrings;
     }
   };
 })();
 
 // GLOBAL APP CONTROLLER
 var controller = (function(budgetCtrl, UICtrl) {
+  var DOM = UICtrl.getDOMstrings();
+
   var ctrlAddItem = function() {
     var input = UICtrl.getinput();
     console.log(input);
   };
 
-  document.querySelector(".add__btn").addEventListener("click", ctrlAddItem);
+  document.querySelector(DOM.inputBtn).addEventListener("click", ctrlAddItem);
 
   document.addEventListener("keydown", function(event) {
     if (event.code === "Enter") {
