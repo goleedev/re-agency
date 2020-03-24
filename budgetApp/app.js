53a8458d0aecca8830e3a434027430e63e873bca
GO Lee <52603436+goleedev@users.noreply.github.com>
2020-03-24 19:58:16 +0900

    Clear input fields

--- budgetApp/app.js
+++ budgetApp/app.js
@@ -89,6 +89,22 @@ var UIController = (function() {
       document.querySelector(element).insertAdjacentHTML("beforeend", newHtml);
     },
 
+    clearFields: function() {
+      var fields, fieldsArr;
+
+      fields = document.querySelectorAll(
+        DOMstrings.inputDescription + ", " + DOMstrings.inputValue
+      );
+
+      fieldsArr = Array.prototype.slice.call(fields);
+
+      fieldsArr.forEach(function(current, index, array) {
+        current.value = "";
+      });
+
+      fieldsArr[0].focus();
+    },
+
     getDOMstrings: function() {
       return DOMstrings;
     }
@@ -114,6 +130,7 @@ var controller = (function(budgetCtrl, UICtrl) {
     input = UICtrl.getinput();
     newItem = budgetCtrl.addItem(input.type, input.description, input.value);
     UICtrl.addListItem(newItem, input.type);
+    UICtrl.clearFields();
   };
 
   return {
